4c2c2b88fe11f3406df9f253a0fe3dc56a63b6aa
hchin157 <30272343+hchin157@users.noreply.github.com>
2017-08-01 11:35:56 -0400

    Update aboutme.js

--- aboutme.js
+++ aboutme.js
@@ -1,4 +1,12 @@
 
-function myFunction() {
+function alertm() {
   alert("Hello there!");
+}
+
+function bgChangeA() {
+  document.body.style.background = 'linear-gradient(#92FA51, #14DA64)';
+}
+
+function bgChangeB() {
+  document.body.style.background = 'linear-gradient(#F0F8FF, #4169E1)';
 }
